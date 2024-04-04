d3f983bf07673f171022b7414f46a6a11b86cee5
Wpompeo <wag_pompeo@yahoo.com.br>
2024-04-04 19:04:12 -0300

    mostrar mensagem para preencher campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -10,25 +11,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Recuperar os componentes EditText
-        //Criar variavel e associar o componente de UI<editText>
-        //Recuperar o botão da tela
-
-        //Colocar ação no botão setOnClickListener
-        //Recuperar o texto digitado no edt peso
+         /*
+          Recuperar os componentes EditText
+          Criar variavel e associar o componente de UI<editText>
+          Recuperar o botão da tela
+          Colocar ação no botão setOnClickListener
+          Recuperar o texto digitado no edt peso
+         */
 
         val edtWeight = findViewById<TextInputEditText>(R.id.edit_weight)
         val edtHeight = findViewById<TextInputEditText>(R.id.edit_height)
         val btnCalculate = findViewById<Button>(R.id.btn_calculate)
 
         btnCalculate.setOnClickListener{
-            val weight: Float = edtWeight.text.toString().toFloat()
-            val height: Float = edtHeight.text.toString().toFloat()
 
-            val heightQ2 = height * height
-            val result = weight / heightQ2
-            println("Action button $result")
+            val weightStr: String = edtWeight.text.toString()
+            val heightStr: String = edtHeight.text.toString()
 
+            if(weightStr == "" || heightStr == ""){
+                Snackbar.make(
+                    edtWeight,
+                    "Preencha todos os campos!",
+                    Snackbar.LENGTH_LONG
+                )
+                .show()
+            } else {
+                val weight = weightStr.toFloat()
+                val height = heightStr.toFloat()
+
+                val heightQ2 = height * height
+                val result = weight / heightQ2
+
+                println("Button action " + result)
+            }
         }
     }
 }
